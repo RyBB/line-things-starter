8cb006d4b4f71937cb75b1c250728b37c8723e6b
BB <ryo-otake@cybozu.co.jp>
2019-03-23 18:55:00 +0900

    Update liff.js

--- liff-app/liff.js
+++ liff-app/liff.js
@@ -98,7 +98,6 @@ eval("// User service UUID: Change this to your generated service UUID\nvar USER
 /***/ })
 
 /******/ });
-=======
 const kintoneJSSDK = require('./kintone-js-sdk.min.js');
 // User service UUID: Change this to your generated service UUID
 const USER_SERVICE_UUID         = 'd6d379f5-91b7-4563-ae20-5e02edec0ec2'; // LED, Button
